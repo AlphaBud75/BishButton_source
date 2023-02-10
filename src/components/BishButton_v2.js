f448266be337bef061eb09399acb39dbab133af6
AlphaBud75 <121747045+alphabud77@users.noreply.github.com>
2023-02-10 13:52:05 -0500

    feb10

--- src/components/BishButton_v2.js
+++ src/components/BishButton_v2.js
@@ -1,34 +1,114 @@
 import { useState, useEffect, useRef } from "react";
 import RollingDigits from "./RollingDigits";
 import './BishButton.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
 export default function BishButton_v2() {
+    let intervalUpdateId;
+    const endpoint = 'https://1c104v13x0.execute-api.us-east-1.amazonaws.com/v1/counter';
+    const
+        [counter_display, setCounter_display] = useState(0),        // counter_local
+        [increment_display, setIncrement_display] = useState(0),    // incrementation
+        [increment_tobe_posted, setIncrement_tobe_posted] = useState(0),    // New Variable
 
-    const [ennaIsAngry, setEnnaIsAngry] = useState(true);
+        getFrequency = 30324500;     // frequency of getting latest counter
 
+    // Update once when mount
+    useEffect(
+        () => {
+            updater(endpoint, 'GET', null)
+                .then((data) => {
+                    console.log('initial-get/fetched. counter: ', data);
+                    setCounter_display(data);
+                }).catch((error) => {
+                    console.error(error);
+                })
+        },
+        []
+    );
+
+    // keep polling. restart when btn clicked
+    useEffect(
+        () => {
+            intervalUpdateId = setInterval(
+                () => {
+                    if (true) {
+                        updater(endpoint, 'GET', null)
+                            .then((data) => {
+                                console.log('\tre-get/fetched. counter: ', data);
+                                // setCounter_local(data);
+                            }).catch((error) => {
+                                console.error(error);
+                            });
+                    }
+                    else if (true) {
+
+                    }
+                }, getFrequency
+            );
+            return () => {
+                clearInterval(intervalUpdateId);
+            };
+        }
+        , []
+    );
+
+
+    /////////////////////////////////////////////////
     return <>
-        <h1>asdfadsf</h1>
-
-
+        Total Bishes:
         <div className="rd mx-auto">
-            <RollingDigits fromParent={999}></RollingDigits>
+            <RollingDigits fromParent={counter_display}></RollingDigits>
         </div>
 
+        Bishes you contributed:
+        <div className="rd mx-auto">
+            &nbsp;<div id="incremention-container">{increment_display}</div>&nbsp;
+        </div>
+        
         <button id="increment-btn"
+            onClick={onClickHandler}
         >
-            <img src={
-                ennaIsAngry
-                    ?
-                    require("../img/Angry.png")
-                    :
-                    require("../img/Normal.png")
-            }></img>
+            increment
         </button>
+        
+        {/* <h3> {!flag ? 'Timedout. keep bishing to resume.' : null} </h3> */}
+        
+        <br />
 
-    </>
+    </>;
+    /////////////////////////////////////////////////////
+
+
+
+
+    function onClickHandler() {
+        // console.log(Date.now() - last_time_bish_long.current);
+        // mouse_click_time.current = Date.now()
+        setCounter_display(x => x + 1);
+        setIncrement_display(x => x + 1);
+        console.log('onClickHandler() : clicked ', increment_display);
+    };
+
+
+    // POST or GET, with payload, to communicate 
+    async function updater(url, method, body) {
+        const response = await fetch(url, {
+            method: method,
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'omit',
+            headers: { 'Content-Type': 'text/plain', },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: body && JSON.stringify(body)
+        });
+        if (method === 'GET')
+            return response.json()
+        return response;
+    };
 
 }
 
